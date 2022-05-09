1860a6a06b0deb95595613d5a2f0b48db4eb7dbd
Kenua <jgvezon@gmail.com>
2022-05-09 06:41:36 -0500

    Add Gameboard factory function
    Added Gameboard factory function and its test file.

---
+++ dist/gameboard.js
@@ -0,0 +1,242 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _ship = _interopRequireDefault(require("./ship.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const Gameboard = function () {
+  let _board = [];
+  let _ships = {
+    type1: {
+      ships: [],
+      length: 5,
+      max: 1
+    },
+    type2: {
+      ships: [],
+      length: 4,
+      max: 2
+    },
+    type3: {
+      ships: [],
+      length: 3,
+      max: 7
+    },
+    type4: {
+      ships: [],
+      length: 2,
+      max: 5
+    }
+  }; // create 10 rows and 10 cells for _board
+
+  for (let row = 0; row < 10; row++) {
+    _board.push([]);
+
+    for (let cell = 0; cell < 10; cell++) {
+      _board[row].push("~");
+    }
+  }
+
+  const getBoard = function () {
+    return JSON.parse(JSON.stringify(_board));
+  };
+
+  const getShips = function () {
+    let shipsClone = {};
+
+    for (let key in _ships) {
+      shipsClone[key] = {};
+      shipsClone[key].ships = [];
+      shipsClone[key].length = _ships[key].length;
+      shipsClone[key].max = _ships[key].max;
+
+      _ships[key].ships.forEach(ship => {
+        let clone = (0, _ship.default)(ship.getLength(), ship.getCoors());
+
+        for (let i = 0; i < ship.getHits(); i++) {
+          clone.hit();
+        }
+
+        shipsClone[key].ships.push(clone);
+      });
+    }
+
+    return shipsClone;
+  };
+
+  const getBoardAndShips = function () {
+    let boardCopy = this.getBoard();
+
+    for (let type in _ships) {
+      for (let i = 0; i < _ships[type].ships.length; i++) {
+        let currentShip = _ships[type].ships[i];
+        let shipCoors = currentShip.getCoors();
+        shipCoors.forEach(coors => {
+          let [row, column] = coors;
+
+          if (boardCopy[row][column] === "~") {
+            boardCopy[row][column] = "s";
+          }
+        });
+      }
+    }
+
+    return boardCopy;
+  };
+
+  const placeShip = function (coordinates = [0, 0], length = 2, orientation) {
+    if (isNaN(Number(coordinates[0])) || isNaN(Number(coordinates[1]))) {
+      throw new Error("Coordinates should be numbers");
+    }
+
+    if (isNaN(Number(length)) || length > 5 || length < 2) {
+      throw new Error("Length should be a number between 2 and 5");
+    }
+
+    let shipCoordinates = [[...coordinates]]; // generate coordinates that expand based on length and orientation
+
+    for (let i = 0; i < length - 1; i++) {
+      // expand coordinates vertically
+      if (orientation === "ver") {
+        let coorsCopy = [...shipCoordinates[i]];
+        coorsCopy[0]++;
+        shipCoordinates.push(coorsCopy); // expand coordinates horizontally
+      } else {
+        let coorsCopy = [...shipCoordinates[i]];
+        coorsCopy[1]++;
+        shipCoordinates.push(coorsCopy);
+      }
+    } // check if shipCoordinates are valid
+
+
+    for (let i = 0; i < shipCoordinates.length; i++) {
+      let currentCoor = shipCoordinates[i];
+      if (currentCoor[0] > 9 || currentCoor[0] < 0) throw new Error("Ship expands to wrong coordinates");
+      if (currentCoor[1] > 9 || currentCoor[1] < 0) throw new Error("Ship expands to wrong coordinates");
+    }
+
+    let newShip = (0, _ship.default)(length, shipCoordinates); // check if newShip can be added to _ships
+
+    for (let type in _ships) {
+      if (_ships[type].length === newShip.getLength()) {
+        if (_ships[type].ships.length < _ships[type].max) {
+          // check every ship's coordinates to see if newShip does not have
+          // the same coordinates of another ship
+          for (let type in _ships) {
+            _ships[type].ships.forEach(ship => {
+              ship.getCoors().forEach(coorsArr1 => {
+                newShip.getCoors().forEach(coorsArr2 => {
+                  if (coorsArr1[0] === coorsArr2[0] && coorsArr1[1] === coorsArr2[1]) {
+                    throw new Error("A new ship cannot be place over another");
+                  }
+                });
+              });
+            });
+          }
+
+          _ships[type].ships.push(newShip);
+
+          return this;
+        } else {
+          let errorMsg = `Exceeded number of ships: maximun number for ${length} length ships is ${_ships[type].max}`;
+          throw new Error(errorMsg);
+        }
+      }
+    }
+  };
+
+  const removeShip = function (row = 0, column = 0) {
+    let filteredShips;
+    let coors;
+
+    for (let type in _ships) {
+      // search and filter out ship that has "row" and "column" as coordinates
+      shipsLoop: for (let i = 0; i < _ships[type].ships.length; i++) {
+        let currentShip = _ships[type].ships[i];
+        let shipCoors = currentShip.getCoors();
+
+        for (let j = 0; j < shipCoors.length; j++) {
+          if (shipCoors[j][0] === row && shipCoors[j][1] === column) {
+            filteredShips = _ships[type].ships.filter(ship => ship !== currentShip);
+            coors = shipCoors;
+            break shipsLoop;
+          }
+        }
+      } // update _ships[type].ships array
+
+
+      if (filteredShips) {
+        let resultMsg = "Removed ship with the following coordinates: ";
+        resultMsg += coors.reduce((acc, current) => acc + `[${current[0]}, ${current[1]}], `, "").slice(0, -2);
+        _ships[type].ships = filteredShips;
+        return resultMsg;
+      }
+    }
+
+    return `There is no ship in [${row},${column}] coordinates`;
+  };
+
+  const isArmyComplete = function () {
+    for (let type in _ships) {
+      if (_ships[type].ships.length < _ships[type].max) return false;
+    }
+
+    return true;
+  };
+
+  const receiveAttack = function (row = 0, column = 0) {
+    let symbol = "m";
+
+    if (_board[row][column] !== "~") {
+      throw new Error(`You already attacked coordinates [${row},${column}]`);
+    } // check if any ship has "row" and "column" as coordinates and hit it
+
+
+    typeLoop: for (let type in _ships) {
+      for (let i = 0; i < _ships[type].ships.length; i++) {
+        let currentShip = _ships[type].ships[i];
+        let shipCoors = currentShip.getCoors();
+
+        for (let j = 0; j < shipCoors.length; j++) {
+          if (shipCoors[j][0] === row && shipCoors[j][1] === column) {
+            currentShip.hit();
+            symbol = "h";
+            break typeLoop;
+          }
+        }
+      }
+    }
+
+    _board[row][column] = symbol;
+    return this;
+  };
+
+  const allShipsSunk = function () {
+    for (let type in _ships) {
+      for (let i = 0; i < _ships[type].ships.length; i++) {
+        if (!_ships[type].ships[i].isSunk()) return false;
+      }
+    }
+
+    return true;
+  };
+
+  return {
+    getBoard,
+    getShips,
+    getBoardAndShips,
+    placeShip,
+    removeShip,
+    isArmyComplete,
+    receiveAttack,
+    allShipsSunk
+  };
+};
+
+var _default = Gameboard;
+exports.default = _default;
---
+++ src/tests/board.test.js
@@ -0,0 +1,443 @@
+import Gameboard from "../gameboard.js";
+
+describe("Testing board", () => {
+   test("Board should have 10 rows and 10 columns", () => {
+      let gameboard1 = Gameboard();
+
+      expect(gameboard1.getBoard()).toEqual([
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+      ]);
+   });
+});
+
+describe("Placing ships", () => {
+   test("Place and check new ships coordinates", () => {
+      let gameboard2 = Gameboard();
+
+      gameboard2.placeShip([0, 0], 5);
+      gameboard2.placeShip([5, 2], 2, "ver");
+
+      let ship1 = gameboard2.getShips().type1.ships[0];
+      let ship2 = gameboard2.getShips().type4.ships[0];
+
+      expect(ship1.getCoors()).toEqual([
+         [0, 0],
+         [0, 1],
+         [0, 2],
+         [0, 3],
+         [0, 4],
+      ]);
+      expect(ship2.getCoors()).toEqual([
+         [5, 2],
+         [6, 2],
+      ]);
+
+      expect(() => gameboard2.placeShip([9, 9], 2)).toThrow(
+         "Ship expands to wrong coordinates"
+      );
+
+      gameboard2.placeShip([6, 5], 4);
+      gameboard2.placeShip([7, 0], 4, "asdf");
+
+      expect(gameboard2.getShips().type2.ships[0].getCoors()).toEqual([
+         [6, 5],
+         [6, 6],
+         [6, 7],
+         [6, 8],
+      ]);
+      expect(gameboard2.getShips().type2.ships[1].getCoors()).toEqual([
+         [7, 0],
+         [7, 1],
+         [7, 2],
+         [7, 3],
+      ]);
+
+      expect(() => gameboard2.placeShip([8, "hi"], 2)).toThrow(
+         "Coordinates should be numbers"
+      );
+      expect(() => gameboard2.placeShip([0, 7], [])).toThrow(
+         "Length should be a number between 2 and 5"
+      );
+   });
+});
+
+describe("Get placed ships", () => {
+   let gameboard = Gameboard();
+
+   test("Get proper number of ships", () => {
+      gameboard.placeShip([0, 0], 5);
+      gameboard.placeShip([1, 0], 4);
+      gameboard.placeShip([1, 4], 4);
+      gameboard.placeShip([2, 0], 2);
+      gameboard.placeShip([2, 2], 2);
+      gameboard.placeShip([2, 4], 2);
+
+      expect(gameboard.getShips().type1.ships.length).toBe(1);
+      expect(gameboard.getShips().type2.ships.length).toBe(2);
+      expect(gameboard.getShips().type4.ships.length).toBe(3);
+   });
+
+   test("Returned ships are copies not references", () => {
+      let ship1 = gameboard.getShips().type1.ships[0];
+      let ship1Copy = gameboard.getShips().type1.ships[0];
+
+      expect(ship1.getHits()).toBe(0);
+      expect(ship1Copy.getHits()).toBe(0);
+
+      ship1.hit();
+      ship1.hit();
+
+      expect(ship1.getHits()).toBe(2);
+      expect(ship1Copy.getHits()).toBe(0);
+
+      gameboard.receiveAttack(0, 2);
+      ship1 = gameboard.getShips().type1.ships[0];
+
+      expect(ship1.getHits()).toBe(1);
+      expect(ship1Copy.getHits()).toBe(0);
+   });
+});
+
+describe("Get board with ships", () => {
+   test("Print board with ships", () => {
+      let gameboard = Gameboard();
+
+      gameboard.placeShip([0, 0], 5);
+
+      gameboard.placeShip([1, 1], 4, "ver");
+      gameboard.placeShip([6, 0], 4);
+
+      gameboard.placeShip([7, 0], 3, "ver");
+      gameboard.placeShip([7, 2], 3, "ver");
+      gameboard.placeShip([7, 4], 3, "ver");
+      gameboard.placeShip([0, 6], 3);
+      gameboard.placeShip([4, 5], 3, "ver");
+      gameboard.placeShip([8, 7], 3);
+      gameboard.placeShip([5, 8], 3, "ver");
+
+      gameboard.placeShip([9, 6], 2);
+      gameboard.placeShip([1, 9], 2, "ver");
+      gameboard.placeShip([4, 9], 2, "ver");
+      gameboard.placeShip([3, 6], 2);
+      gameboard.placeShip([5, 7], 2, "ver");
+
+      expect(gameboard.getBoardAndShips()).toEqual([
+         ["s", "s", "s", "s", "s", "~", "s", "s", "s", "~"],
+         ["~", "s", "~", "~", "~", "~", "~", "~", "~", "s"],
+         ["~", "s", "~", "~", "~", "~", "~", "~", "~", "s"],
+         ["~", "s", "~", "~", "~", "~", "s", "s", "~", "~"],
+         ["~", "s", "~", "~", "~", "s", "~", "~", "~", "s"],
+         ["~", "~", "~", "~", "~", "s", "~", "s", "s", "s"],
+         ["s", "s", "s", "s", "~", "s", "~", "s", "s", "~"],
+         ["s", "~", "s", "~", "s", "~", "~", "~", "s", "~"],
+         ["s", "~", "s", "~", "s", "~", "~", "s", "s", "s"],
+         ["s", "~", "s", "~", "s", "~", "s", "s", "~", "~"],
+      ]);
+   });
+
+   test("Print board with ships with attacks and missing attacks", () => {
+      let gameboard = Gameboard();
+
+      gameboard.placeShip([0, 0], 5);
+      gameboard.placeShip([1, 1], 4, "ver");
+      gameboard.placeShip([7, 0], 3, "ver");
+      gameboard.placeShip([3, 6], 2);
+
+      gameboard.receiveAttack(0, 0);
+      gameboard.receiveAttack(0, 1);
+      gameboard.receiveAttack(0, 2);
+
+      gameboard.receiveAttack(1, 0);
+      gameboard.receiveAttack(2, 0);
+      gameboard.receiveAttack(3, 0);
+      gameboard.receiveAttack(4, 0);
+      gameboard.receiveAttack(1, 2);
+
+      gameboard.receiveAttack(7, 0);
+      gameboard.receiveAttack(8, 0);
+      gameboard.receiveAttack(9, 0);
+
+      gameboard.receiveAttack(3, 5);
+      gameboard.receiveAttack(3, 9);
+
+      expect(gameboard.getBoardAndShips()).toEqual([
+         ["h", "h", "h", "s", "s", "~", "~", "~", "~", "~"],
+         ["m", "s", "m", "~", "~", "~", "~", "~", "~", "~"],
+         ["m", "s", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["m", "s", "~", "~", "~", "m", "s", "s", "~", "m"],
+         ["m", "s", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["h", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["h", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["h", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+      ]);
+   });
+});
+
+describe("Prevent exceed maximum number of ships", () => {
+   test("Exceed maximum number of 2 length ships", () => {
+      let gameboard3 = Gameboard();
+
+      gameboard3.placeShip([0, 0], 2);
+      gameboard3.placeShip([1, 0], 2);
+      gameboard3.placeShip([2, 0], 2);
+      gameboard3.placeShip([3, 0], 2);
+      gameboard3.placeShip([4, 0], 2);
+
+      expect(() => gameboard3.placeShip([5, 0], 2)).toThrow(
+         "Exceeded number of ships: maximun number for 2 length ships is 5"
+      );
+   });
+});
+
+describe("Place new ships", () => {
+   test("Place all types of ships", () => {
+      let gameboard4 = Gameboard();
+
+      gameboard4.placeShip([0, 0], 5);
+
+      gameboard4.placeShip([1, 0], 4);
+      gameboard4.placeShip([2, 0], 4);
+
+      gameboard4.placeShip([3, 0], 3);
+      gameboard4.placeShip([4, 0], 3);
+      gameboard4.placeShip([5, 0], 3);
+      gameboard4.placeShip([6, 0], 3);
+      gameboard4.placeShip([7, 0], 3);
+      gameboard4.placeShip([8, 0], 3);
+      gameboard4.placeShip([9, 0], 3);
+
+      gameboard4.placeShip([0, 5], 2);
+      gameboard4.placeShip([1, 4], 2);
+      gameboard4.placeShip([2, 4], 2);
+      gameboard4.placeShip([3, 3], 2);
+      gameboard4.placeShip([4, 3], 2);
+
+      expect(() => gameboard4.placeShip([0, 7], 2)).toThrow(
+         "Exceeded number of ships: maximun number for 2 length ships is 5"
+      );
+      expect(() => gameboard4.placeShip([0, 7], 5, "ver")).toThrow(
+         "Exceeded number of ships: maximun number for 5 length ships is 1"
+      );
+   });
+});
+
+describe("Place ship over another ship", () => {
+   test("Prevent a new ship on the same coordinates", () => {
+      let gameboard5 = Gameboard();
+
+      gameboard5.placeShip([0, 0], 5);
+
+      expect(() => gameboard5.placeShip([0, 4], 4)).toThrow(
+         "A new ship cannot be place over another"
+      );
+
+      gameboard5.placeShip([0, 5], 2);
+      gameboard5.placeShip([0, 7], 3);
+
+      expect(() => gameboard5.placeShip([0, 9], 4, "ver")).toThrow(
+         "A new ship cannot be place over another"
+      );
+   });
+});
+
+describe("Remove ships", () => {
+   test("Place and remove all types of ships", () => {
+      let gameboard = Gameboard();
+
+      // place ships
+      gameboard.placeShip([0, 0], 5); // (*)
+
+      gameboard.placeShip([1, 0], 4); // (*)
+      gameboard.placeShip([2, 0], 4);
+
+      gameboard.placeShip([3, 0], 3);
+      gameboard.placeShip([4, 0], 3); // (*)
+      gameboard.placeShip([5, 0], 3);
+      gameboard.placeShip([6, 0], 3);
+      gameboard.placeShip([7, 0], 3);
+      gameboard.placeShip([8, 0], 3);
+      gameboard.placeShip([9, 0], 3);
+
+      gameboard.placeShip([0, 5], 2);
+      gameboard.placeShip([1, 4], 2);
+      gameboard.placeShip([2, 4], 2);
+      gameboard.placeShip([3, 3], 2);
+      gameboard.placeShip([4, 3], 2); // (*)
+
+      // remove ships pointing to the coordinates of the first chunk
+      expect(gameboard.removeShip(0, 0)).toBe(
+         "Removed ship with the following coordinates: [0, 0], [0, 1], [0, 2], [0, 3], [0, 4]"
+      );
+      expect(gameboard.getShips().type1.ships.length).toBe(0);
+
+      expect(gameboard.removeShip(1, 0)).toBe(
+         "Removed ship with the following coordinates: [1, 0], [1, 1], [1, 2], [1, 3]"
+      );
+      expect(gameboard.getShips().type2.ships.length).toBe(1);
+
+      // remove ships by not pointing to the coordinates of the first chunk
+      expect(gameboard.removeShip(4, 1)).toBe(
+         "Removed ship with the following coordinates: [4, 0], [4, 1], [4, 2]"
+      );
+      expect(gameboard.getShips().type3.ships.length).toBe(6);
+
+      expect(gameboard.removeShip(4, 4)).toBe(
+         "Removed ship with the following coordinates: [4, 3], [4, 4]"
+      );
+      expect(gameboard.getShips().type4.ships.length).toBe(4);
+   });
+
+   test("Remove ship that does not exist", () => {
+      let gameboard = Gameboard();
+
+      expect(gameboard.removeShip(0, 0)).toBe(
+         "There is no ship in [0,0] coordinates"
+      );
+   });
+});
+
+describe("Checking army", () => {
+   let gameboard = Gameboard();
+
+   test("Army is not complete", () => {
+      expect(gameboard.isArmyComplete()).toBe(false);
+   });
+
+   test("Army is complete", () => {
+      gameboard.placeShip([0, 0], 5);
+
+      gameboard.placeShip([1, 0], 4);
+      gameboard.placeShip([2, 0], 4);
+
+      gameboard.placeShip([3, 0], 3);
+      gameboard.placeShip([4, 0], 3);
+      gameboard.placeShip([5, 0], 3);
+      gameboard.placeShip([6, 0], 3);
+      gameboard.placeShip([7, 0], 3);
+      gameboard.placeShip([8, 0], 3);
+      gameboard.placeShip([9, 0], 3);
+
+      gameboard.placeShip([0, 5], 2);
+      gameboard.placeShip([1, 4], 2);
+      gameboard.placeShip([2, 4], 2);
+      gameboard.placeShip([3, 3], 2);
+      gameboard.placeShip([4, 3], 2);
+
+      expect(gameboard.isArmyComplete()).toBe(true);
+   });
+});
+
+describe("Attack board", () => {
+   let gameboard = Gameboard();
+
+   test("Hit a single ship", () => {
+      gameboard.placeShip([0, 0], 5);
+
+      gameboard.receiveAttack(0, 4);
+      expect(gameboard.getBoard()).toEqual([
+         ["~", "~", "~", "~", "h", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+      ]);
+      expect(gameboard.getShips().type1.ships[0].getHits()).toBe(1);
+   });
+
+   test("Miss an attack", () => {
+      gameboard.receiveAttack(0, 5);
+      expect(gameboard.getBoard()).toEqual([
+         ["~", "~", "~", "~", "h", "m", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+      ]);
+   });
+
+   test("Attack same coordinates", () => {
+      expect(() => gameboard.receiveAttack(0, 4)).toThrow(
+         "You already attacked coordinates [0,4]"
+      );
+      expect(() => gameboard.receiveAttack(0, 5)).toThrow(
+         "You already attacked coordinates [0,5]"
+      );
+   });
+
+   test("Sunk a ship", () => {
+      gameboard.receiveAttack(0, 0);
+      gameboard.receiveAttack(0, 1);
+      gameboard.receiveAttack(0, 2);
+      gameboard.receiveAttack(0, 3);
+      expect(gameboard.getBoard()).toEqual([
+         ["h", "h", "h", "h", "h", "m", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+         ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+      ]);
+
+      expect(gameboard.getShips().type1.ships[0].getHits()).toBe(5);
+      expect(gameboard.getShips().type1.ships[0].isSunk()).toBe(true);
+   });
+});
+
+describe("Check if all ships were sink", () => {
+   test("All ships sunk", () => {
+      let gameboard1 = Gameboard();
+
+      gameboard1.placeShip([0, 0], 5);
+      gameboard1.placeShip([1, 0], 4);
+      gameboard1.placeShip([2, 0], 4);
+
+      expect(gameboard1.allShipsSunk()).toBe(false);
+
+      gameboard1.receiveAttack(0, 0);
+      gameboard1.receiveAttack(0, 1);
+      gameboard1.receiveAttack(0, 2);
+      gameboard1.receiveAttack(0, 3);
+      gameboard1.receiveAttack(0, 4);
+
+      expect(gameboard1.allShipsSunk()).toBe(false);
+
+      gameboard1.receiveAttack(1, 0);
+      gameboard1.receiveAttack(1, 1);
+      gameboard1.receiveAttack(1, 2);
+      gameboard1.receiveAttack(1, 3);
+
+      expect(gameboard1.allShipsSunk()).toBe(false);
+
+      gameboard1.receiveAttack(2, 0);
+      gameboard1.receiveAttack(2, 1);
+      gameboard1.receiveAttack(2, 2);
+      gameboard1.receiveAttack(2, 3);
+
+      expect(gameboard1.allShipsSunk()).toBe(true);
+   });
+});
